0e2a48fccf45d19da1119c1fc7d9634d0e415e36
RJ Trenchard <rjtrenchard@gmail.com>
2024-01-18 12:54:08 -0500

    add styling

--- tmp/263390vjekisw.16mt3q1hb61x6.php
+++ tmp/263390vjekisw.16mt3q1hb61x6.php
@@ -1,16 +1,18 @@
 <!doctype HTML>
 <html lang="en">
     <head>
-        <meta charset="UTF8">
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pet Home</title>
         <link rel="stylesheet" href="styles/home.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
-    <body>
-    <div class="container bg-light">
+    <body class="bg-light">
+    <div class="container">
         <h1>Pet Home</h1>
-        <p><a class="btn btn-primary" href="https://github.com/rjtrenchard/pets" target="_blank">Github link</a></p>
+        <p>
+            <a class="btn btn-primary my-button" href="https://github.com/rjtrenchard/pets" target="_blank">Github link</a>
+        </p>
         <img class="img-fluid" src="images/pets.jpg" alt="picture of Moki">
     </div>
     </body>
